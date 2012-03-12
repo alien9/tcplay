60cc539eb048afceb188fc95c5f04701867d6ca2
Tiago Barufi <tiago@MacBook.local>
2012-03-12 02:37:18 -0300

    comeco

---
+++ tcplay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tcplay
+//
+//  Created by Tiago Barufi on 12/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tcplayTests/tcplayTests.h
@@ -0,0 +1,13 @@
+//
+//  tcplayTests.h
+//  tcplayTests
+//
+//  Created by Tiago Barufi on 12/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface tcplayTests : SenTestCase
+
+@end
