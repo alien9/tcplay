f891e7be0667a95b46f48bfdd9b6cea3e7515484
Tiago Barufi <barufi@gmail.com>
2012-03-12 02:40:49 -0300

    php de volta

---
+++ backend/config.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+switch($_SERVER['SERVER_NAME']){
+case 'localhost':
+	//Local
+	$dsn = "mysql:localhost;port=3306;dbname=tcplay";
+	$usuario = "tcplay";
+	$senha = "K4VyDnU8Y7xx4rTW";	
+break;
+case 'metlife.dromedario':
+case 'metlife.mantis':
+case 'metlife.this':
+	//Local
+	$dsn = "mysql:localhost;port=3306;dbname=metlife";
+	$usuario = "metlife";
+	$senha = "NHabaN7SRUS4aJ7y";	
+	$opcoes="";
+break;
+default:
+	//Produção
+}
+
+$opcoes = array(
+    PDO::ATTR_PERSISTENT => false,
+    PDO::ATTR_CASE => PDO::CASE_LOWER
+);
+
+$pdo = new PDO($dsn, $usuario, $senha, $opcoes);
+$pdo->exec('SET NAMES utf8'); 
---
+++ backend/juke.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once("config.inc.php");
+
+$s=$pdo->prepare("select * from jukebox");
+$s->execute();
+echo json_encode($s->fetchAll());
