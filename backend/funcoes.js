b6a39f395362f5ff868733f013d59fc01650957d
Leandro Manuel Reis Velloso <leandro@MacBook.local>
2012-03-14 20:05:40 -0300

    backend finished

--- backend/funcoes.js
+++ backend/funcoes.js
@@ -1,57 +1,9 @@
-function toogleDiv(nome){
-	$('#'+nome).slideToggle('slow', function() {
-		// Animation complete.
+// FUN√á√ÉO COM JQUERY PARA ATIVAR E DESATIVAR REGISTROS
+function ativarDesativar(tab){
+	$("#td_ativar img").live("mouseover", function(){ $('body').css('cursor', 'pointer'); });
+	$("#td_ativar img").live("mouseout", function(){ $('body').css('cursor', 'default'); });
+	$("#td_ativar img").live("click", function(){
+			var id = $(this).attr("id");
+			$(".td_ativar"+id).load('ativar.php?id='+id+'&tb='+tab);
 	});
-}
-function gravaMail(nome,campo){
-	OK = true;
-	if ((nome=="")||(nome=="nome")) {
-		alert ("Digite seu nome.")
-		OK = false
-		return;	
-	}	
-	if (campo.search(/^\w+((-\w+)|(\.\w+))*\@\w+((\.|-)\w+)*\.\w+$/) == -1) {
-		alert ("O e-mail digitado n„o È v·lido.")
-		OK = false
-		return;	
-	}
-	if (OK == true) {
-			$.ajax({
-				url: "grava_mail.php",
-				data: "nome="+$("#nome").attr('value')+"&campo_especial_hb="+$("#campo_especial_hb").attr('value'),
-				cache: false,
-				success: function(html){ $("#msg").html('E-mail cadastrado!'); }
-			});
-	 }
-}
-function enviaMail(campo){
-	OK = true;
-	if (document.getElementById('nome').value.length < 2) {
-		alert ("Preencha seu nome.")
-		OK = false
-		return;	
-	}
-	if ($('#campo_especial_hb2').attr('value').search(/^\w+((-\w+)|(\.\w+))*\@\w+((\.|-)\w+)*\.\w+$/) == -1) {
-		alert ("O e-mail digitado n„o È v·lido.")
-		OK = false
-		return;	
-	}
-	if (document.getElementById('assunto').value.length < 2) {
-		alert ("Preencha o assunto.")
-		OK = false
-		return;	
-	}		
-	if (document.getElementById('mensagem').value.length < 2) {
-		alert ("Preencha a mensagem.")
-		OK = false
-		return;	
-	}			
-	if (OK == true) {
-			$.ajax({
-				url: "envia_mail.php",
-				data: "nome="+$("#nome").attr('value')+"&campo_especial_hb2="+$("#campo_especial_hb2").attr('value')+"&assunto="+$("#assunto").attr('value')+"&mensagem="+$("#mensagem").attr('value'),
-				cache: false,
-				success: function(html){ $("#aviso").html('Sua mensagem foi enviada com sucesso!'); }
-			});
-	 }
 }
--- backend/premiados.php
+++ backend/premiados.php
@@ -3,22 +3,29 @@ include_once("header.php");
 if($_REQUEST['ordena']){ $ordena = $_REQUEST['ordena']; }else{ $ordena = 'id'; };
 ?>
 
+<script language="javascript">
+	ativarDesativar('ped');								 
+</script>
+
 <h1>Lista de músicas</h1>
 
 <table class="table_listas">
   <tr height="20">
     <th width="40"><a href="?ordena=id">Id</a></th>
+    <th><a href="?ordena=sorteado">Nome</a></th>
+    <th><a href="?ordena=setor">Operação / Regional</a></th>
     <th><a href="?ordena=artist">Artista</a></th>
     <th><a href="?ordena=title">Música</a></th>
-    <th><a href="?ordena=pedidos DESC">Pedidos</a></th>
+    <th><a href="?ordena=hora DESC">Hora</a></th>
+    <th width="40"><a href="?ordena=pedidos DESC">Entregue</a></th>
   </tr>
   <?
   
   $sql = "
-  SELECT j.id, j.artist, j.title, j.ativo, COUNT(p.id) AS pedidos
-  FROM jukebox j
-  LEFT JOIN pedidos p ON p.id_song=j.id
-  GROUP BY j.id
+  SELECT p.id, p.sorteado, p.setor, j.artist, j.title, p.retirado, p.hora
+  FROM pedidos p
+  LEFT JOIN jukebox j ON p.id_song=j.id
+  WHERE p.sorteado != ''
   ORDER BY ".$ordena." 
   ";
   $stmt = $pdo->query($sql);
@@ -29,9 +36,17 @@ if($_REQUEST['ordena']){ $ordena = $_REQUEST['ordena']; }else{ $ordena = 'id'; }
 		?>
     <tr class="<?=$cor_tr?>" onmouseover="this.className='tr_over'" onmouseout="this.className ='<?=$cor_tr?>'" height="20">
       <td align="center"><?=$r["id"]?></td>
+      <td><?=$r["sorteado"]?></td>
+      <td><?=$r["setor"]?></td>
       <td><?=$r["artist"]?></td>
       <td><?=$r["title"]?></td>
-      <td><? if($r["pedidos"]){ echo $r["pedidos"]; } ?></td>
+      <td><?=date('H:i:s', strtotime($r["hora"]));?></td>
+      <td align="center" id="td_ativar" class="td_ativar<?=$r["id"]?>">
+		<? 
+		
+		echo $r["retirado"] ? "<img id='".$r["id"]."' src='img/ico_ativo.gif' />" : "<img id='".$r["id"]."' src='img/ico_inativo.gif' />"; 
+        ?>
+      </td>
     </tr>								
     <?	
   }
