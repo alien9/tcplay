a3567b0ebda5682ac7d45f0f3bcdab81d45fd595
Leandro Velloso <leandro@candy.(none)>
2012-03-14 11:43:12 -0300

    generos

---
+++ backend/string_insere_todas.php
@@ -0,0 +1,9 @@
+<?
+
+
+INSERT INTO 'tcplay'.'jukebox' ('title', 'artist', 'genero', 'premiado') VALUES 
+('LEVELS', 'AVICII', 'DANCE MUSIC', '0'), 
+('TAKE OVER CONTROL', 'AFROJACK', 'DANCE MUSIC', '0'),
+;
+
+?>
