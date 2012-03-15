aa17f5e89f1f4715575a96e01c9bd1d795bf9b37
Tiago Barufi <barufi@gmail.com>
2012-03-15 19:28:22 -0300

    erro besta corrigido - ou melhor, não era erro

--- backend/reset.php
+++ backend/reset.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once("config.inc.php");
-$s=$pdo->prepare("update jukebox set ip=null");
+$s=$pdo->prepare("update jukebox set ip=null,ativo=1");
 $s->execute();
 $s=$pdo->prepare("TRUNCATE pedidos");
 $s->execute();
