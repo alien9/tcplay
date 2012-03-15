a5205bc14987ac7f605940eee63b9199db342816
Tiago Barufi <barufi@gmail.com>
2012-03-15 00:48:06 -0300

    php_from_hell

--- backend/generos.php
+++ backend/generos.php
@@ -2,6 +2,6 @@
 
 include_once("config.inc.php");
 
-$s=$pdo->prepare("select DISTINCT generos from jukebox");
+$s=$pdo->prepare("select DISTINCT genero from jukebox");
 $s->execute();
 echo json_encode($s->fetchAll());
--- backend/juke.php
+++ backend/juke.php
@@ -2,6 +2,27 @@
 
 include_once("config.inc.php");
 
-$s=$pdo->prepare("select * from jukebox");
+$s=$pdo->prepare("select * from jukebox where premiado=0");
 $s->execute();
-echo json_encode($s->fetchAll());
+$o=$s->fetchAll();
+
+$ip=$_SERVER['REMOTE_ADDR'];
+
+$s=$pdo->prepare("select * from jukebox where premiado=1 and ip=? and ativo=1");
+$s->execute(array($ip));
+$p=$s->fetchAll();
+
+if(!count($p)){
+  $s=$pdo->prepare("select * from jukebox where premiado=1 and ativo=1 and ip is null order by rand()");
+  $s->execute();
+  $i=0;
+  while(($r=$s->fetch())&&($i<26)){
+    $t=$pdo->prepare("update jukebox set ip=? where premiado=1 and id=?");
+    $t->execute(array($ip,$r['id']));
+    array_push($o,$r);
+    $i++;
+  }
+}else{
+  $o=array_merge($o,$p);
+}
+echo json_encode($o);
