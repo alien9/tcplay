c5f22d787fa1ded765ca683eca990a66756d43d1
Tiago Barufi <barufi@gmail.com>
2012-03-15 03:53:49 -0300

    resetando

--- backend/reset.php
+++ backend/reset.php
@@ -1,9 +1,7 @@
 <?php
 
 include_once("config.inc.php");
-
-error_log(print_r($_REQUEST,1));
-
 $s=$pdo->prepare("update jukebox set ip=null");
 $s->execute();
-
+$s=$pdo->prepare("TRUNCATE pedidos");
+$s->execute();
