698915c90cb4a748040ac3ea87593ded19cd0483
Tiago Barufi <tiago@MacBook-2.local>
2012-08-13 19:07:14 -0300

    problematic

---
+++ tcplay/SongButton.m
@@ -0,0 +1,31 @@
+//
+//  SongButton.m
+//  tcplay
+//
+//  Created by Tiago Barufi on 14/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SongButton.h"
+
+@implementation SongButton
+@synthesize song;
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
